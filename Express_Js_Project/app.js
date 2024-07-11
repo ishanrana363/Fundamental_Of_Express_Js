e844674f799c371a6bb9ded36e84cae8fc5e4883
ishanrana363 <ishanrana363@gmail.com>
2024-07-11 20:02:45 +0600

    security middleware import & implementation

---
+++ Express_Js_Project/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = new express();
+
+// security middleware
+
+const expressRateLimit =  require("express-rate-limit");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const cors = require("cors");
+const hpp = require("hpp");
+const limiter = expressRateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+});
+
+app.use(limiter);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(hpp());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+module.exports = app;
+
